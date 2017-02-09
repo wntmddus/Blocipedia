058f2b0cd0c8707743f3702277939b3269385a93
Seung Yeon Joo <wntmddussla@hotmail.com>
2017-02-09 00:25:00 -0800

    checkpoint 2 and 3

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root to: 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
